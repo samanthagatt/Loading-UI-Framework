df223a214e7f659e85000a7412435ef36036c5d6
Samantha Gatt <smfgatt@gmail.com>
2018-09-19 15:14:13 -0400

    Creates framework and example app

---
+++ LoadingUIFramework/LoadingUIFramework/LoadingUIFramework.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUIFramework.h
+//  LoadingUIFramework
+//
+//  Created by Samantha Gatt on 9/19/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoadingUIFramework.
+FOUNDATION_EXPORT double LoadingUIFrameworkVersionNumber;
+
+//! Project version string for LoadingUIFramework.
+FOUNDATION_EXPORT const unsigned char LoadingUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUIFramework/PublicHeader.h>
+
+
